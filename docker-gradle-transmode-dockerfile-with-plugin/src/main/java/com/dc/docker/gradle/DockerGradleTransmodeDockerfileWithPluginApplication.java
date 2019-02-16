226b2f830c01df39a6fb38000a99a68ec64d1273
anwaaralikhan <manwaaronline@gmail.com>
2019-02-15 23:28:30 -0500

    Dockerfile with Transmode Plugin

---
+++ docker-gradle-transmode-dockerfile-with-plugin/src/main/java/com/dc/docker/gradle/DockerGradleTransmodeDockerfileWithPluginApplication.java
@@ -0,0 +1,14 @@
+package com.dc.docker.gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerGradleTransmodeDockerfileWithPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerGradleTransmodeDockerfileWithPluginApplication.class, args);
+	}
+
+}
+
